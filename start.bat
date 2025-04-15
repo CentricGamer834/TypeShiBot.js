8cdcc85861c20f3501a8f615451003a8d3cdaafe
Centric <theminecrafter13772@gmail.com>
2025-04-14 21:39:12 -0400

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,1 +1,8 @@
+@ECHO OFF
+echo installing requirements
+npm install
+echo requirements instlaled
+echo make sure to paste your Discord user or bot token into typeshibot.js before starting!
+echo starting bot
 npm start
+pause
